1a9aa222a2ebf8ca29845e59bb838c896f44aada
iamtesch <iamtesch@2f259f02-18c0-11df-9eea-1f1cb9bda66b>
2010-02-18 19:12:49 +0000

    1. Added more comments, and removed unneccessary functions. 2. Cemented the heirarchical structure for objects. 3. Corrected the redraw on resize. 4. Implemented a stem object for vertical lines that connect to noteheads.  The stem object supports chords (including drawing them), but the recognition system does not yet.

--- MusicObjects.py
+++ MusicObjects.py
@@ -7,6 +7,7 @@ TYPE_NONE = 0
 TYPE_STAFF = 1
 TYPE_NOTE = 2
 TYPE_BARLINE = 3
+TYPE_STEM = 4
 
 NOTE_FILLED = -1
 NOTE_EMPTY = -2
@@ -14,31 +15,53 @@ NOTE_EMPTY = -2
 BARLINE_NORMAL = 1
 
 class MusicObject:
+	"""
+	  A base class for musical objects (WITH semantic meaning; i.e., not just a
+	  circle, but a note; not just a line, but a stem or a barline).
+	"""
 	def __init__(self,pos):
 		self.position = pos
 		self.type = TYPE_NONE
 	def draw(self,canvas,scale):
 		pass
-	def resize(self,width,scale):
-		pass
 	def dist(self,point):
 		return sqrt(pow(self.position[0]-point[0],2)+pow(self.position[1]-point[1],2))
 
 class Staff(MusicObject):
+	"""
+	  Staves are the base object that is drawn on the page.  They contain notes,
+	  clefs, and various other musical symbols, and most other markings are
+	  children of a staff.
+	"""
 	def __init__(self,pos,width):
 		MusicObject.__init__(self,pos)
 		self.width = width
 		self.type = TYPE_STAFF
 		self.objects = []
+
 	def draw(self,canvas,scale):
+		"""
+		  Draw the staff and all its children (notes, barlines, clefs, etc.)
+		"""
+		# Draw staff
 		for i in [-2,-1,0,1,2]:
 			h = (i*STAFFSPACING+self.position[1])*scale
 			pygame.draw.line(canvas, BLACK, (0,h), (self.width,h), 1*scale)
-	# For staves, the vertical distance is all that matters.
+		# Draw children
+		for obj in self.objects:
+			obj.draw(canvas,scale)
+
+	# For staves, "distance" is just the vertical distance to the center
 	def dist(self,point):
 		return abs(self.position[1]-point[1])
+
+	# This function returns the closest line the the given point (with zero
+	# being the center of the staff)
 	def whichLine(self,y):
 		return int(round(2.0*(y - self.position[1])/(STAFFSPACING)))
+
+	# These remaining functions add notes and other objects to the list of
+	# children of the staff.
 	def addNote(self,note):
 		self.objects.append(note)
 	def addObject(self,obj):
@@ -57,37 +80,121 @@ class Barline(MusicObject):
 		t = self.parent.position[1] + int((STAFFSPACING/2.0)*scale*(-4))
 		b = self.parent.position[1] + int((STAFFSPACING/2.0)*scale*(4))
 		pygame.draw.line(canvas,pygame.Color("black"),(x,t),(x,b),2)
+	# For barlines, distance is the horizontal distance to the line
+	def dist(self,point):
+		return abs(self.position[0]-point[0])
 
-# This is a notehead object.  It should really just be a notehead, and not
-# representative of anything more.
 class Note(MusicObject):
-	def __init__(self,pos,length,parent):
+	"""
+	  The notehead object represents a single notehead.  It's parent is either a
+	  stem or a staff.
+
+	  When the parent is a staff, the note is a "free" note.  The x position is
+	  the position from the left side of the page,
+	
+	  When the parent is a stem, the note is attached to that stem (possibly
+	  with other notes).  The x position now is either -1, to represent
+	  attachment to the left side of the stem, or +1, to represent attachement
+	  to the right side of the stem.
+	
+	  In either case, the y position is the line or space number of the staff
+	  that is either its parent or grandparent.
+	"""
+	def __init__(self,pos,parent,length,side=-1):
 		MusicObject.__init__(self,pos)
 		self.length = length
 		self.parent = parent
-		self.position[1] = parent.whichLine(pos[1])
 		self.type = TYPE_NOTE
+		if self.parent.type == TYPE_STAFF:
+			self.position[1] = parent.whichLine(pos[1])
+		if self.parent.type == TYPE_STEM:
+			self.position[1] = parent.parent.whichLine(pos[1])
+			self.position[0] = side
+
 	def draw(self,canvas,scale):
-		x = self.position[0]
-		y = self.parent.position[1] + int((STAFFSPACING/2.0)*scale*self.position[1])
+		"""
+		  Because the x and y positions have different semantic meanings if the
+		  parent is a staff or a stem, these cases are handled differently here.
+		"""
+		if self.parent.type == TYPE_STAFF:
+			x = self.position[0]
+			y = self.parent.position[1] + int((STAFFSPACING/2.0)*scale*self.position[1])
+		elif self.parent.type == TYPE_STEM:
+			side = self.position[0]
+			x = self.parent.position[0] + (STAFFSPACING/2.0)*scale*side;
+			y = self.parent.parent.position[1] + int((STAFFSPACING/2.0)*scale*self.position[1])
+
 		if self.length == NOTE_FILLED:
 			pygame.draw.circle(canvas,pygame.Color("black"),[x,y], 8)
-		if self.length == NOTE_EMPTY:
+		elif self.length == NOTE_EMPTY:
 			pygame.draw.circle(canvas,pygame.Color("black"),[x,y], 8, 2)
+
 	def dist(self,point):
+		"""
+		  The distance function here will return the distance, in page
+		  coordinates, to the center of the note.
+		"""
 		x = self.position[0]-point[0]
 		y = self.parent.position[1] + int((STAFFSPACING/2.0)*self.position[1]) - point[1]
 		return sqrt(x*x+y*y)
 
 # This is a stem for a notehead.  It's parent is the chord it attaches to.
 class Stem(MusicObject):
-	def __init__(self,pos):
-		MusicObject.__init__(self,pos)
+	"""
+	  A stem is a vertical line, attached to one or more notes.  It can either
+	  be pointing up from the bottom note of the chord, on the right side
+	  (except for clusters), or can be pointing down from the top note of the
+	  chord, on the left side (again, note clusters must be handled as a
+	  special case).
 
-# Chords contain one or more notes, as well as stems, accents, rhythm dots, etc.
-class Chord(MusicObject):
-	def __init__(self,pos):
+	  The x and y position coordinates act like free notes; in other words, the
+	  x position is the page coordinate of the stem, from the left side of the
+	  page, and the y position is the line or space at which the stem begins.
+	"""
+	def __init__(self,pos,parent,length,direction,children):
 		MusicObject.__init__(self,pos)
+		# -1 represents a down stem, +1 represents an up stem. 
+		self.direction = direction
+		# The length of the stem, in number of lines and spaces
+		self.length = length
+		# The note or notes that this stem attaches to.
+		self.notes = children
+		# The parent staff which this stem belongs to.
+		self.parent = parent
+		self.type = TYPE_STEM
+
+		# During initialization, the stem must set parameters of its new
+		# children, and remove them from the staff's set of children
+		for note in self.notes:
+			# Set the x position of the notes.  In general, this is the left
+			# side for up-stems, and the left side for down-stems.
+			#TODO figure out logic for determing stem side for clusters of notes
+			note.position[0] = -self.direction
+
+			# Remove the note from its parent staff
+			note.parent.objects.remove(note)
+
+			# Change the note's parent to this stem
+			note.parent = self
+
+	def draw(self,canvas,scale):
+		for note in self.notes:
+			note.draw(canvas,scale)
+		x = self.position[0]
+		line = self.position[1]
+		base = self.parent.position[1] + int((STAFFSPACING/2.0)*scale*(line))
+		end = base-scale*STAFFSPACING/2.0*self.length*self.direction
+		pygame.draw.line(canvas,pygame.Color("black"),(x,base),(x,end),2)
+
+	def dist(self,point):
+		"""
+		  The distance function here will return the distance, in page
+		  coordinates, to the base of the stem.
+		"""
+		x = self.position[0]-point[0]
+		y = self.parent.position[1] + int((STAFFSPACING/2.0)*self.position[1]) - point[1]
+		return sqrt(x*x+y*y)
+			
 
 def getClosest(objects,point,type = TYPE_ANY):
 	dist = inf
@@ -97,13 +204,3 @@ def getClosest(objects,point,type = TYPE_ANY):
 			dist = object.dist(point)
 			best = object
 	return best, dist
-
-def getClosestNoteOnLine(objects,xpos,staff,line):
-	dist = inf
-	best = None
-	for object in objects:
-		if object.type == TYPE_NOTE and object.parent == staff and object.position[1] == line and abs(xpos-object.position[0]) < dist:
-			dist = abs(xpos-object.position[0])
-			best = object
-	return best
-			
