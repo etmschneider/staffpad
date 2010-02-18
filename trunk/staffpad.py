c00a03e94b3c1ae1728f95c9f466bc985624be4c
iamtesch <iamtesch@2f259f02-18c0-11df-9eea-1f1cb9bda66b>
2010-02-18 17:17:37 +0000

    Major structure change and code cleanup to enable class-based structure, and improve readability.  Also, changed heirarchy of objects, pad, and pages.

--- trunk/staffpad.py
+++ trunk/staffpad.py
@@ -2,182 +2,297 @@ import pygame
 from numpy import *
 import MusicObjects
 import Symbols
-pygame.init()
 
-# Global vars: --------------------------------------
-# Default window size:
-width = 512
-height = 512
-
-def makeBackground(width, height):
+def makeBackground(size):
 	"""
-	Make our backround surface
+	  Non-transparent surface where the content is displayed
 	"""
-	background = pygame.surface.Surface((width, height))
+	background = pygame.surface.Surface(size)
 	background.fill(pygame.Color("white"))
 	background = background.convert()
 	return background
 
-def main():
+def makeOverlay(size):
 	"""
-	Simple Pygame to show off tablet pressure sensitivity.
+	  Transparent overlay surface where ink can be temporarily stored
 	"""
-	# Pull in globals, since they can be modified due to screen resize
-	global width
-	global height
-
-	radius = 1
-	zoom = 1
-	objects = []
-
-	screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-	pygame.display.set_caption("StaffPad v0.0");
-	background = makeBackground(width, height)
-	for i in range(4):
-		s = MusicObjects.Staff((0,100*i+110),width)
-		objects.append(s)
-		s.draw(background,zoom);
-#		for j in range(5):
-#			h = 15*j+100*i+60
-#			pygame.draw.line(background, pygame.Color("black"), (0,h), (width,h), linewidth)
-	screen.blit(background, (0,0))
-
-	overlay = pygame.surface.Surface(screen.get_size(), flags=pygame.SRCALPHA, depth=32)
+	overlay = pygame.surface.Surface(size, flags=pygame.SRCALPHA, depth=32)
 	overlay.convert_alpha()
 	overlay.fill((0,0,0,0))
+	return overlay
 
-	mouseSurf = pygame.surface.Surface((radius*2,radius*2),flags=pygame.SRCALPHA, depth=32)
-	mouseSurf.convert_alpha()
-	mouseSurf.fill((0,0,0,0))
+# This function redraws all of the objects onto the background at a given size
+def redraw(width,height,zoom):
+	pass
 
+class Page:
+	def __init__(self,pad):
+		"""
+		  This is a "page" of staff paper.  It has staves, which can contain
+		  notes and other symbols.
+		
+		  It shares common attributes, such as page size, with all the other
+		  pages in the pad.
+		"""
+		self.pad = pad
 
-	xPos, yPos = pygame.mouse.get_pos()
-	xPrev = xPos
-	yPrev = yPos
+		# A list of the staves on the page
+		# TODO: Eventually, these will probably become systems instead
+		self.staves = []
 
-	drawing = 0;
-	shape = [];
+		# Initialize the page with four staves
+		for i in range(4):
+			s = MusicObjects.Staff((0,100*i+110),self.pad.pageSize[0])
+			self.staves.append(s)
 
-	# Main loop:
-	looping = True
-	while looping:
-		events = pygame.event.get()
-		for event in events:
-			if event.type == pygame.QUIT:
-				# allow for exit:
-				looping = False
-			# If the window is resized, update the Pygame screen, and update
-			# the area of our tablet:
-			if event.type == pygame.VIDEORESIZE:
-				width, height = event.size
-				screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-				# Copy our old background, to apply that image to the new one:
-				oldBg = background.copy()
-				background = makeBackground(width, height)
-				background.blit(oldBg, (0,0))
-				oldOv = overlay.copy()
-				overlay = makeBackground(width, height)
-				background.blit(oldOv, (0,0))
+	def addObject(self,type,rect):
+		"""
+		  This function attempts to contain all of the logic for determining
+		  what a given shape (line, circle, etc) actually means musically.
+		  This is probably too much for one function, and should eventually be
+		  broken up into smaller chunks in some intelligent way.
+		"""
+		# Conditions to become a note:
+		# - shape must be a circle or a dot
+		# - TODO: size must be in the expected range for a note
+		if type == 'circle' or type == 'dot':
+			center = [0.5*(rect[0][0]+rect[1][0]),0.5*(rect[0][1]+rect[1][1])]
 
-		#overlay.fill((0,0,0,0))
+			# get closest staff to attach note to
+			staff, dist = MusicObjects.getClosest(self.staves,center)
 
+			# make note object
+			if type == 'dot':
+				n = MusicObjects.Note(center,MusicObjects.NOTE_FILLED,staff)
+			elif type == 'circle':
+				n = MusicObjects.Note(center,MusicObjects.NOTE_EMPTY,staff)
+
+			# attach note to staff, and draw it TODO: should we trigger a redraw
+			# instead?
+			staff.addNote(n)
+			n.draw(self.pad.background,self.pad.zoom)
+
+		elif type == 'vline':
+			center = [0.5*(rect[0][0]+rect[1][0]),0.5*(rect[0][1]+rect[1][1])]
+			top = rect[0][1]
+			bottom = rect[1][1]
+			right = rect[1][0]
+			left = rect[0][0]
+
+			# Find the closest staff
+			staff, dist = MusicObjects.getClosest(self.staves,center)
+
+			# Find the lines which the line starts and ends at
+			endlines = [staff.whichLine(top),staff.whichLine(bottom)]
+
+			# Find the closest notes to the top and bottom of the line
+			topNote, topDist = MusicObjects.getClosest(staff.objects,(right,top),MusicObjects.TYPE_NOTE)
+			botNote, botDist = MusicObjects.getClosest(staff.objects,(left,bottom),MusicObjects.TYPE_NOTE)
+
+			# If the vertical line's top or bottom is close to a note,
+			# then we make it a stem of that note, giving preference
+			# to the closest note (bottom if they are equal)
+			if min(botDist,topDist) < MusicObjects.STAFFSPACING*0.75:
+				if botDist <= topDist:
+					print "stem for note at bottom of line"
+					#TODO: make stem attach to bottom note
+				else:
+					print "stem for note at top of line"
+					#TODO: make stem attach to top note
+
+			# Otherwise, for it to be a barline, it must start and end
+			# at the staff's top and bottom line
+			# TODO: Change these values to a staff constant, based on 
+			# staff type
+			elif (endlines[0] in [-3,-4,-5] and endlines[1] in [3,4,5]):
+				barline = MusicObjects.Barline(center[0],staff)
+				# draw barline
+				staff.addObject(barline)
+				barline.draw(self.pad.background,self.pad.zoom);
+			else:
+				print "unrecognized vertical line"
+		else:
+			print "unhandled shape"
+
+class StaffPad:
+	def __init__(self,width=512,height=512):
+		"""
+		  Initialize the pad of staff paper.  The pad consists of a collection
+		  of pages.  Also, the pad contains the information about what the
+		  user is looking at (i.e., which page and area the screen should
+		  display).
+		"""
+		# The current zoom setting.
+		self.zoom = 1.0
+		# The number of pixels of drawn lines at a unit zoom setting.
+		self.radius = 1.0
+
+		# Initialize the screen, background, and overlays
+		self.resizeScreen([width,height])
+		pygame.display.set_caption("StaffPad v0.0");
+
+		# This is the page size (represents maximum width of staves)
+		# TODO: logic of resizing the page.  Bigger is fairly simple, smaller
+		# may cause clipping, which should be handled nicely.
+		self.pageSize = [width,height]
+
+		# The mouse surface is a small patch where a orange "dot" cursor is 
+		# displayed while drawing objects, to feel more like an ink-aware
+		# program.  This cursor scales with the zoom.
+		self.mouseSurface = makeOverlay((self.zoom*self.radius*2,self.zoom*self.radius*2))
+
+		# Add an initial page
+		self.pages = [];
+		self.pages.append(Page(self))
+
+		# Set the current page you are looking at.
+		self.currentPage = 0
+
+		# Draw the initial staves onto the screen.
+		self.redraw()
+
+	def resizeScreen(self,newSize):
+		"""
+		  Called during initialization or resizing of the screen, and builds the
+		  background to display data, and the overlay surface to write ink on.
+		"""
+		# This is the screen size.  The current screen should be scrollable to
+		# view the entire page.  The maximum size should be the current page
+		# size times the zoom factor.
+		# TODO: add scroll bars.  Until then, the top left corner of the screen
+		# will always coincide with the top left corner of the page.
+		self.screenSize = newSize
+
+		# The pygame screen on which everything is drawn
+		self.screen = pygame.display.set_mode(self.screenSize, pygame.RESIZABLE)
+
+		# The background is the surface where recognized objects are displayed
+		self.background = makeBackground(self.screenSize)
+
+		# The overlay is the surface where the "ink" is displayed as objects
+		# are drawn
+		self.overlay = makeOverlay(self.screenSize)
+
+	def run(self):
+		"""
+		  This is the main loop which captures and analyzes input, and displays
+		  objects on the screen.
+		"""
+
+		### Initialize variables that help with capturing gestures.
+
+		# Track the mouse position
+		xPos, yPos = pygame.mouse.get_pos()
 		xPrev = xPos
 		yPrev = yPos
-		xPos, yPos = pygame.mouse.get_pos()
 
-		# If user is pressing the button 0, or button 3, (LMB, RMB) draw:
-		mouseButtons = pygame.mouse.get_pressed()
-		if mouseButtons[0]:
-			drawing = 1
-			#if len(shape) == 0 or abs(shape[-1][0]-xPos) + abs(shape[-1][1]-yPos) > 1:
-			if len(shape) == 0 or shape[-1][0] != xPos or shape[-1][1] != yPos:
-				shape.append([xPos,yPos])
-			#pygame.draw.circle(overlay, pygame.Color("black"), (xPos,yPos), radius)
-			pygame.draw.line(overlay, pygame.Color("black"), (xPrev,yPrev), (xPos,yPos), radius*2)
-		else:
-			# If we just finished, analyze the shape...
-			if drawing == 1:
+		# drawing describes whether a shape is currently being captured, so that
+		# sepecial actions can be taken at the start and end of the gestures
+		drawing = 0;
+
+		# stores the coordinates of points along the drawn path
+		shape = [];
+
+		# Main loop for capturing input
+		looping = True
+		while looping:
+			# Catch any major events (quit button pressed, screen resize, etc)
+			events = pygame.event.get()
+			for event in events:
+				# This is caused by pressing the "X" in the top right corner
+				if event.type == pygame.QUIT:
+					# Causing the while loop to stop, and quits the program.
+					# Alternatively, this could be caught with a "do you want
+					# to save" message.
+					looping = False
+				# If the window is resized, update the Pygame screen, the
+				# screen area, and cue a redraw
+				if event.type == pygame.VIDEORESIZE:
+					self.resizeScreen(event.size)
+					self.redraw()
+
+			# Update the tracked mouse position
+			xPrev = xPos
+			yPrev = yPos
+			xPos, yPos = pygame.mouse.get_pos()
+
+			# Get mouse buttons (detect whether stylus is touching)
+			mouseButtons = pygame.mouse.get_pressed()
+
+			# This means the left mouse button is down, which corresponds to
+			# the stylus touching.
+			if mouseButtons[0]:
+				# Set flag to indicate that a shape is being recorded
+				drawing = 1
+				# Add a new point, unless the cursor hasn't moved
+				if len(shape) == 0 or shape[-1][0] != xPos or shape[-1][1] != yPos:
+					shape.append([xPos,yPos])
+				# Draw the corresponding line segment on the overlay
+				pygame.draw.line(self.overlay, pygame.Color("black"), (xPrev,yPrev), (xPos,yPos), self.radius*2)
+
+			# As soon as the button is lifted, classify the gesture, and add the
+			# object.
+			elif drawing == 1:
+				# We are no longer drawing; turn off the flag
 				drawing = 0
-				#print "analyzing shape!"
-				#print shape
-				#shape = array(shape)
-				type = Symbols.classify(shape);
-				print type
-				pos = Symbols.center(shape);
-				if type == 'circle' or type == 'dot':
-					# correct position according to closest staff
-					close_staff = MusicObjects.get_closest(objects,pos,MusicObjects.TYPE_STAFF)
-					# make note object
-					if type == 'dot':
-						n = MusicObjects.Note(pos,MusicObjects.NOTE_FILLED,close_staff)
-					elif type == 'circle':
-						n = MusicObjects.Note(pos,MusicObjects.NOTE_EMPTY,close_staff)
 
-					# draw note
-					objects.append(n)
-					n.draw(background,zoom);
-				elif type == 'vline':
-					top = amin(shape,0)[1]
-					bottom = amax(shape,0)[1] 
-					#TODO: maybe there should be a top and bottom staff?
-					close_staff = MusicObjects.get_closest(objects,pos,MusicObjects.TYPE_STAFF)
-					topline = close_staff.which_line(top,zoom);
-					bottomline = close_staff.which_line(bottom,zoom);
+				# classify the gesture into a shape
+				type = Symbols.classify(shape)
 
-					topNote = MusicObjects.getClosestNoteOnLine(objects,pos[0],close_staff,topline)
-					bottomNote = MusicObjects.getClosestNoteOnLine(objects,pos[0],close_staff,bottomline)
-					topNoteDist = Inf
-					bottomNoteDist = Inf
-					if topNote and abs(pos[0]-topNote.position[0]) < MusicObjects.STAFFSPACING*0.75*zoom:
-						topNoteDist = abs(pos[0]-topNote.position[0])
-					if bottomNote and abs(pos[0]-bottomNote.position[0]) < MusicObjects.STAFFSPACING*0.75*zoom:
-						bottomNoteDist = abs(pos[0]-bottomNote.position[0])
+				# get a bounding rectangle for this shape
+				rect = Symbols.boundingBox(shape)
 
-					# If the vertical line's top or bottom is close to a note,
-					# then we make it a stem of that note, giving preference
-					# to the closest note (bottom if they are equal)
-					if min(bottomNoteDist,topNoteDist) < Inf:
-						if bottomNoteDist <= topNoteDist:
-							print "stem for note at bottom of line"
-							#TODO: make stem attach to bottom note
-						else:
-							print "stem for note at top of line"
-							#TODO: make stem attach to top note
+				# Convert screen coordinates to page coordinates!
+				pageRect = self.screenToPage(rect)
 
-					# Otherwise, for it to be a barline, it must start and end
-					# at the staff's top and bottom line
-					# TODO: Change these values to a staff constant, based on 
-					# staff type
-					elif (topline == -4 and bottomline == 4):
-						barline = MusicObjects.Barline(pos[0],close_staff)
-						# draw barline
-						objects.append(barline)
-						barline.draw(background,zoom);
-					else:
-						print "unrecognized vertical line"
-					
+				# Given the classified shape, we must now determine what it
+				# semantically means.  For example, is a vertical line a barline
+				# or a note stem?
+				# This is done at the page level, which means the coordinates
+				# passed in to this function should be page coordinates.
+				self.pages[self.currentPage].addObject(type,pageRect)
 
-				# erase ink
-				# pygame.draw.rect(overlay, (0,0,0,0), (0,0), (width,height));
-				overlay.fill((0,0,0,0))
+				# erase the ink from the gesture, and reset the shape
+				self.overlay.fill((0,0,0,0))
 				shape = []
-		if mouseButtons[1]:
-			#pygame.draw.circle(overlay, pygame.Color("white"), (xPos,yPos), radius)
-			#pygame.draw.line(overlay, pygame.Color("white"), (xPrev,yPrev), (xPos,yPos), radius*2)
-			pygame.draw.line(overlay, (0,0,0,0), (xPrev,yPrev), (xPos,yPos), radius*4)
 
-		# Draw our mouse pointer representation:
-		pygame.draw.circle(mouseSurf, pygame.Color("orange"), (radius,radius), radius)
+			# This means the eraser is touching.  Currently, we don't do
+			# anything in this case.
+			if mouseButtons[1]:
+				pass
 
-		screen.blit(background, (0,0))
-		screen.blit(overlay, (0,0))
-		screen.blit(mouseSurf, (xPos-radius,yPos-radius))
-		#screen.blit(mouseSurf, (0,0))
-		pygame.display.flip()
+			# Draw our mouse pointer representation:
+			pygame.draw.circle(self.mouseSurface, pygame.Color("orange"), (self.radius,self.radius), self.radius)
 
-	pygame.quit()
+			# Blit (write) all of the background and overlay data to the screen 
+			self.screen.blit(self.background, (0,0))
+			self.screen.blit(self.overlay, (0,0))
+			self.screen.blit(self.mouseSurface, (xPos-self.zoom*self.radius,yPos-self.zoom*self.radius))
+			pygame.display.flip()
+		pygame.quit()
 
+	# This takes a list of points in screen coordinates, and converts to a list
+	# in page coordinates.
+	def screenToPage(self,pointsIn):
+		pointsOut = []
+		for p in pointsIn:
+			pointsOut.append([p[0]/self.zoom,p[1]/self.zoom])
+		return pointsOut
 
+	def redraw(self):
+		"""
+		  This function redraws the objects on screen after a zoom, scroll, or
+		  resize.  It is also responsible for the intial drawing of objects.
+		"""
+		# TODO: do we have to adjust the background size to be as big as the
+		# scaled page size, and then only display part of it on the screen? If
+		# this isn't done, will higher zooms or small screens cause objects to
+		# be drawn off of the background, causing errors?
+		# TODO: can we just do scrolling by adjusting the blit point?
+		for staff in self.pages[self.currentPage].staves:
+			staff.draw(self.background, self.zoom)
+		self.screen.blit(self.background, (0,0))
 
-if __name__ == "__main__":
-	main()
+pygame.init()
+pad = StaffPad()
+pad.run()
