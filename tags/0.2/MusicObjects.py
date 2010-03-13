ba560abba6b307e100ed131ecb9d12f3959900d1
iamtesch <iamtesch@2f259f02-18c0-11df-9eea-1f1cb9bda66b>
2010-03-13 14:54:38 +0000

    Verson 0.2 is complete, and is now tagged.

---
+++ tags/0.2/MusicObjects.py
@@ -0,0 +1,432 @@
+import pygame
+from numpy import *
+STAFFSPACING = 15.0
+BLACK = pygame.Color("black")
+TYPE_ANY = -1
+TYPE_NONE = 0
+TYPE_STAFF = 1
+TYPE_NOTE = 2
+TYPE_BARLINE = 3
+TYPE_STEM = 4
+
+NOTE_FILLED = -1
+NOTE_EMPTY = -2
+
+BARLINE_NORMAL = 1
+
+class MusicObject:
+	"""
+	  A base class for musical objects (WITH semantic meaning; i.e., not just a
+	  circle, but a note; not just a line, but a stem or a barline).  The
+	  base class just acts like a single point with no meaning, and should not
+	  be used by itself.
+	"""
+	def __init__(self,pos):
+		self.position = list(pos)
+		self.type = TYPE_NONE
+		self.rect = pygame.Rect(0,0,0,0)
+	def draw(self,canvas,scale):
+		pass
+	def dist(self,point):
+		return sqrt(pow(self.position[0]-point[0],2)+pow(self.position[1]-point[1],2))
+	def dist2(self,point):
+		"""
+		  Returns the horizontal and vertical distances from the object to the
+		  given point.  Should return signed distances.
+		"""
+		return [self.position[0]-points[0],self.position[1]-point[1]]
+	def isUnder(self,point):
+		"""
+		  This function determines whether the given point is over the object,
+		  which is useful, for example, in determining whether to erase it.
+		"""
+		return False
+	def isIn(self,rect):
+		"""
+		  This functions returns 1 if some part of the object is within the
+		  given rectangle.
+		"""
+		return (rect[0][0] < self.position[0] < rect[1][0]) and (rect[0][1] < self.position[1] < rect[1][1])
+
+class Staff(MusicObject):
+	"""
+	  Staves are the base object that is drawn on the page.  They contain notes,
+	  clefs, and various other musical symbols, and most other markings are
+	  children of a staff.
+	"""
+	def __init__(self,pos,width):
+		MusicObject.__init__(self,pos)
+		self.width = width
+		self.type = TYPE_STAFF
+		self.objects = []
+		height = STAFFSPACING*4.0
+		self.rect = pygame.Rect(pos[0],pos[1]-height/2.0,width,height)
+
+	def draw(self,canvas,scale):
+		"""
+		  Draw the staff and all its children (notes, barlines, clefs, etc.)
+		"""
+		# Draw staff
+		for i in [-2,-1,0,1,2]:
+			h = int((i*STAFFSPACING+self.position[1])*scale)
+			pygame.draw.line(canvas, BLACK, (0,h), (self.width,h), int(1.0*scale))
+		# Draw children
+		for obj in self.objects:
+			obj.draw(canvas,scale)
+
+	# For staves, "distance" is just the vertical distance to the center
+	def dist(self,point):
+		return abs(self.position[1]-point[1])
+
+	def dist2(self,points):
+		return [0,self.position[1]-point[1]]
+
+	# This function returns the closest line the the given point (with zero
+	# being the center of the staff)
+	def whichLine(self,y):
+		return int(round(2.0*(y - self.position[1])/(STAFFSPACING)))
+
+	# These remaining functions add notes and other objects to the list of
+	# children of the staff.
+	def addNote(self,note):
+		self.objects.append(note)
+	def addObject(self,obj):
+		self.objects.append(obj)
+
+	def removeAt(self,point):
+		"""
+		  Remove any objects at the given point.  For notes, remove any accents
+		  or accidentals associated with them.  For barlines, simply remove the
+		  barline.  For stemmed notes: remove only the stem if the point is
+		  over the stem, only the note if it is over one note in a multi-note
+		  chord (readjusting stem length if necessary), and both if the point
+		  is over the notehead on a single note with stem.
+	
+		  Return value is True if objects were removed, false if not.
+		"""
+		removed = False
+		for object in self.objects:
+			if object.isUnder(point):
+				# Remove free notes and barlines
+				if object.type == TYPE_NOTE or object.type == TYPE_BARLINE:
+					self.objects.remove(object)
+					removed = True
+				# Remove notes from stem or stem from notes (or both)
+				if object.type == TYPE_STEM:
+					object.removeAt(point)
+					removed = True
+		return removed
+
+class Barline(MusicObject):
+	def __init__(self,xpos,parent):
+		MusicObject.__init__(self,(xpos,0))
+		self.parent = parent
+		# For barlines, the horizontal distance is all that matters
+		self.position = (xpos,0);
+		self.type = TYPE_BARLINE
+		self.style = BARLINE_NORMAL
+		self.rect = pygame.Rect(xpos-1,self.parent.rect.top,2,self.parent.rect.h)
+	def draw(self,canvas,scale):
+		x = self.position[0]
+		t = self.parent.position[1] + int((STAFFSPACING/2.0)*scale*(-4))
+		b = self.parent.position[1] + int((STAFFSPACING/2.0)*scale*(4))
+		pygame.draw.line(canvas,pygame.Color("black"),(x,t),(x,b),2)
+	# For barlines, distance is the horizontal distance to the line
+	def dist(self,point):
+		return abs(self.position[0]-point[0])
+	def isUnder(self,point):
+		return self.rect.collidepoint(point)
+
+class Note(MusicObject):
+	"""
+	  The notehead object represents a single notehead.  It's parent is either a
+	  stem or a staff.
+
+	  When the parent is a staff, the note is a "free" note.  The x position is
+	  the position from the left side of the page,
+	
+	  When the parent is a stem, the note is attached to that stem (possibly
+	  with other notes).  The x position now is either -1, to represent
+	  attachment to the left side of the stem, or +1, to represent attachement
+	  to the right side of the stem.
+	
+	  In either case, the y position is the line or space number of the staff
+	  that is either its parent or grandparent.
+	"""
+	def __init__(self,pos,parent,length,side=-1):
+		MusicObject.__init__(self,pos)
+		self.length = length
+		self.parent = parent
+		self.type = TYPE_NOTE
+		if self.parent.type == TYPE_STAFF:
+			self.position[1] = parent.whichLine(pos[1])
+		if self.parent.type == TYPE_STEM:
+			self.position[1] = parent.parent.whichLine(pos[1])
+			self.position[0] = side
+		self.setRect
+
+	def setRect(self):
+		if self.parent.type == TYPE_STAFF:
+			x = self.position[0]
+			y = self.parent.position[1] + (STAFFSPACING/2.0)*self.position[1]
+		elif self.parent.type == TYPE_STEM:
+			side = self.position[0]
+			x = self.parent.position[0] + (STAFFSPACING/2.0)*scale*side;
+			y = self.parent.parent.position[1] + (STAFFSPACING/2.0)*self.position[1]
+		self.rect = pygame.Rect(x-STAFFSPACING/2.0,y-STAFFSPACING/2.0,STAFFSPACING,STAFFSPACING)
+
+	def draw(self,canvas,scale):
+		"""
+		  Because the x and y positions have different semantic meanings if the
+		  parent is a staff or a stem, these cases are handled differently here
+		"""
+		if self.parent.type == TYPE_STAFF:
+			x = int(self.position[0])
+			y = self.parent.position[1] + int((STAFFSPACING/2.0)*scale*self.position[1])
+			staffMiddle = self.parent.position[1]
+		elif self.parent.type == TYPE_STEM:
+			side = self.position[0]
+			x = int(self.parent.position[0] + (STAFFSPACING/2.0)*scale*side);
+			y = self.parent.parent.position[1] + int((STAFFSPACING/2.0)*scale*self.position[1])
+			staffMiddle = self.parent.parent.position[1]
+
+		if self.length == NOTE_FILLED:
+			pygame.draw.circle(canvas,pygame.Color("black"),[x,y], 8)
+		elif self.length == NOTE_EMPTY:
+			pygame.draw.circle(canvas,pygame.Color("black"),[x,y], 8, 2)
+
+		# Draw ledger lines if the note is...
+		# ...above the staff, or...
+		for line in range(int(ceil(self.position[1]/2.))*2,-4,2):
+			h = int((staffMiddle+(line/2)*STAFFSPACING)*scale)
+			pygame.draw.line(canvas, BLACK, (x-(STAFFSPACING/1.5)*scale,h), (x+(STAFFSPACING/1.5),h), int(1.0*scale))
+		# below the staff
+		for line in range(6,int(self.position[1]/2)*2+2,2):
+			h = int((staffMiddle+(line/2)*STAFFSPACING)*scale)
+			pygame.draw.line(canvas, BLACK, (x-(STAFFSPACING/1.5)*scale,h), (x+(STAFFSPACING/1.5),h), int(1.0*scale))
+
+	def dist(self,point):
+		"""
+		  The distance function here will return the distance, in page
+		  coordinates, to the center of the note.
+		"""
+		if self.parent.type == TYPE_STAFF:
+			x = self.position[0]-point[0]
+			y = self.parent.position[1] + (STAFFSPACING/2.0)*self.position[1]-point[1]
+		elif self.parent.type == TYPE_STEM:
+			side = self.position[0]
+			x = int(self.parent.position[0] + (STAFFSPACING/2.0)*side)-point[0]
+			y = self.parent.parent.position[1] + (STAFFSPACING/2.0)*self.position[1]-point[1]
+		return sqrt(x*x+y*y)
+	def isUnder(self,point):
+		return self.dist(point) < (STAFFSPACING/2.0)
+	def isIn(self,rect):
+		"""
+		  This is actually just a (close) approximation, for computational
+		  efficiency.  To figure out whether an object is in another, you can
+		  check whether a point is in the minkowski sum.  In this case, we
+		  instead just expand the rectangle by the note radius, and check the
+		  center position of the note, which gives (a small number of) false
+		  positives at the corners.
+		"""
+		# Get x and y in page coordinates		
+		if self.parent.type == TYPE_STAFF:
+			x = int(self.position[0])
+			y = self.parent.position[1] + int((STAFFSPACING/2.0)*self.position[1])
+		elif self.parent.type == TYPE_STEM:
+			side = self.position[0]
+			x = int(self.parent.position[0] + (STAFFSPACING/2.0)*side);
+			y = self.parent.parent.position[1] + int((STAFFSPACING/2.0)*self.position[1])
+
+		return ((rect[0][0]-STAFFSPACING*0.5) < x < (rect[1][0]+STAFFSPACING*0.5)) and ((rect[0][1]-STAFFSPACING*0.5) < y < (rect[1][1]+STAFFSPACING*0.5))
+
+class Stem(MusicObject):
+	"""
+	  A stem is a vertical line, attached to one or more notes.  It can either
+	  be pointing up from the bottom note of the chord, on the right side
+	  (except for clusters), or can be pointing down from the top note of the
+	  chord, on the left side (again, note clusters must be handled as a
+	  special case).
+
+	  The x and y position coordinates act like free notes; in other words, the
+	  x position is the page coordinate of the stem, from the left side of the
+	  page, and the y position is the line or space at which the stem begins.
+	"""
+	def __init__(self,pos,parent,length,direction,children):
+		MusicObject.__init__(self,pos)
+		# -1 represents a down stem, +1 represents an up stem. 
+		self.direction = direction
+		# The length of the stem, in number of lines and spaces
+		self.length = length
+		# The note or notes that this stem attaches to.
+		self.notes = children
+		# The parent staff which this stem belongs to.
+		self.parent = parent
+		self.type = TYPE_STEM
+
+		# During initialization, the stem must set parameters of its new
+		# children, and remove them from the staff's set of children
+		for note in self.notes:
+			# Set the x position of the notes.  In general, this is the left
+			# side for up-stems, and the left side for down-stems.
+			#TODO figure out logic for determing stem side for clusters of notes
+			note.position[0] = -self.direction
+
+			# Remove the note from its parent staff
+			note.parent.objects.remove(note)
+
+			# Change the note's parent to this stem
+			note.parent = self
+		self.setRect()
+		self.clusterNotes()
+
+	def setRect(self):
+		x = self.position[0]
+		yTop = self.parent.position[1] + int((STAFFSPACING/2.0)*self.position[1])
+		yBot = yTop
+		if self.direction == 1:
+			yTop -= self.length*STAFFSPACING/2.0
+		else:
+			yBot += self.length*STAFFSPACING/2.0
+		self.rect = pygame.Rect(x-1,yTop,2,yBot-yTop)
+
+	def addNotes(self,notes):
+		for note in notes:
+			# Add new notes to the stem's list of notes, and remove from the
+			# list of their previous parent
+			self.notes.append(note)
+			note.parent.objects.remove(note)
+
+			# Set the x position of the notes.  In general, this is the left
+			# side for up-stems, and the left side for down-stems.
+			note.position[0] = -self.direction
+
+			# Change the note's parent to this stem
+			note.parent = self
+		self.clusterNotes()
+
+	def draw(self,canvas,scale):
+		for note in self.notes:
+			note.draw(canvas,scale)
+		x = self.position[0]
+		line = self.position[1]
+		base = self.parent.position[1] + int((STAFFSPACING/2.0)*scale*(line))
+		end = base-scale*STAFFSPACING/2.0*self.length*self.direction
+		pygame.draw.line(canvas,pygame.Color("black"),(x,base),(x,end),2)
+
+	def dist(self,point):
+		"""
+		  The distance function here will return the distance, in page
+		  coordinates, to the closest part of the stem.
+		"""
+		x = self.position[0]-point[0]
+		yTop = self.parent.position[1] + int((STAFFSPACING/2.0)*self.position[1])
+		yBot = yTop
+		if self.direction == 1:
+			yTop -= self.length*STAFFSPACING/2.0
+		else:
+			yBot += self.length*STAFFSPACING/2.0
+		if yTop < point[1] < yBot:
+			y = 0
+		else:
+			y = min(abs(point[1] - yTop),abs(point[1] - yBot))
+
+		return sqrt(x*x+y*y)
+
+	def isUnder(self,point):
+		"""
+		  Is any part of the stem or connected notes under the given point?
+		"""
+		if self.rect.collidepoint(point):
+			return True
+		for note in self.notes:
+			if note.isUnder(point):
+				return True
+		return False
+
+	def removeAt(self,point):
+		"""
+		  This is called if a stem or stemmed note collides with the given point
+		  and has to be removed.
+		"""
+		removeMe = []
+		for note in self.notes:
+			if note.isUnder(point):
+				removeMe.append(note)
+		for note in removeMe:
+			self.notes.remove(note)
+
+		# If we collide with the stem or have erased all the notes, delete the
+		# stem
+		if self.rect.collidepoint(point) or len(self.notes) == 0:
+			self.parent.objects.remove(self)
+			for note in self.notes:
+				self.parent.objects.append(note)
+				note.parent = self.parent
+				note.position[0] = self.position[0] + (STAFFSPACING/2.0)*note.position[0];
+		# recompute stem location, length, and any clusters
+		else:
+			# the base note is the lowest on the page (highest y)
+			if self.direction == 1:
+				maxPos = -inf
+				for note in self.notes:
+					maxPos = max(note.position[1],maxPos)
+				self.length -= self.position[1]-maxPos
+				self.position[1] = maxPos
+			# the base note is the highest on the page (lowest y)
+			else:
+				minPos = inf
+				for note in self.notes:
+					minPos = min(note.position[1],minPos)
+				self.length -= minPos-self.position[1]
+				self.position[1] = minPos
+			#clusters the remaining notes correctly
+			self.clusterNotes()
+
+	def clusterNotes(self):
+		"""
+			This function computes the correct x-position (left or right of the
+			stem) for each note in a chord, taking into account the effect of
+			"clustered" notes (ones a second apart)
+		"""
+		noteList = {}
+		for note in self.notes:
+			noteList[note.position[1]] = note
+		order = noteList.keys()
+		order.sort()
+
+		# For a down-stem:
+		if self.direction == -1:
+			noteList[order[0]].position[0] = -self.direction
+			for i in range(1,len(order)):
+				if (order[i-1] == order[i]-1):
+					noteList[order[i]].position[0] = -noteList[order[i-1]].position[0]
+				else:
+					noteList[order[i]].position[0] = -self.direction
+		# For an up-stem:
+		else:
+			noteList[order[-1]].position[0] = -self.direction
+			for i in range(1,len(order)):
+				if (order[-i] == order[-i-1]+1):
+					noteList[order[-i-1]].position[0] = -noteList[order[-i]].position[0]
+				else:
+					noteList[order[-i-1]].position[0] = -self.direction
+
+def getClosest(objects,point,type = TYPE_ANY):
+	dist = inf
+	best = None
+	for object in objects:
+		if (type == TYPE_ANY or object.type == type) and object.dist(point) < dist:
+			dist = object.dist(point)
+			best = object
+	return best, dist
+
+def getObjectsIn(objects,rect,type = TYPE_ANY):
+	"""
+	  Return all objects of a given type that are in the given rectangle.
+	"""
+	inside = []
+	for object in objects:
+		if (type == TYPE_ANY or object.type == type) and object.isIn(rect):
+			inside.append(object)
+	return inside
---
+++ tags/0.2/Symbols.py
@@ -0,0 +1,166 @@
+# This file is dedicated to the classification of shapes drawn by the user into
+# a set of symbols allowed by the program.  The individual symbols are stored in
+# the symbols directory, and they have a template file for each size.
+
+import numpy as np
+from math import pow, sqrt, pi, exp
+
+def center(shape):
+	mnpts = np.amin(shape,0)
+	mxpts = np.amax(shape,0)
+	w = mxpts[0] - mnpts[0] + 1
+	h = mxpts[1] - mnpts[1] + 1
+	return [mnpts[0]+w/2,mnpts[1]+h/2]
+
+def boundingBox(shape):
+	mnpts = np.amin(shape,0)
+	mxpts = np.amax(shape,0)
+	return (mnpts,mxpts)
+	
+def classify(shape):
+	"""
+	  This function contains the logic to classify the shape into a basic
+	  symbol; i.e., a vertical line, empty circle, filled circle, sharp, flat,
+	  etc.
+	  This is done by first sorting by size and aspect ratio, and then comparing	  the object to a list of approprate symbols templates.  These symbol
+	  templates are smaller grids that represent the density of pixels in a
+	  larger binary image.
+
+	  After comparing to the existing templates, a score is computed, and the
+	  highest score is chosen.
+	"""
+	# Shape is a set of x/y coordinates:
+	shape = np.array(shape)
+	mnpts = np.amin(shape,0)
+	mxpts = np.amax(shape,0)
+	w = mxpts[0] - mnpts[0] + 1
+	h = mxpts[1] - mnpts[1] + 1
+
+	# Transform into a binary image
+	shapeBinary = np.zeros([h,w],int)
+	for i in range(np.size(shape,0)):
+		shapeBinary[shape[i,1]-mnpts[1],shape[i,0]-mnpts[0]] = 1;
+
+	# Pick appropriate templates using aspect ratio
+	# TODO: could size play a part here, too?
+	aspectRatios = [(5,5),(10,1)]
+	templates = [['dot','circle'],['vline']]
+	ratioScore = []
+	for ratio in aspectRatios:
+		wt = ratio[1]
+		ht = ratio[0]
+		ratioScore.append((w*wt+h*ht)/sqrt(w*w+h*h)/sqrt(wt*wt+ht*ht))
+	if max(ratioScore) < 0.85:
+		#print "No matching aspect ratio"
+		return 'unclassified'
+
+	ratioNum = ratioScore.index(max(ratioScore))
+	ratio = aspectRatios[ratioNum]
+	templates = templates[ratioNum]
+	
+	# Transform the shape to the (currently standard) template size
+	shapeDensity = densityTransform(shapeBinary,ratio)
+
+	# Compute a score for each template
+	scores = []
+	for name in templates:
+		templateMean = np.load('symbols/' + name + '-' + str(ratio[0]) + 'x' + str(ratio[1]) + '-mean.npy')
+		templateStd = np.load('symbols/' + name + '-' + str(ratio[0]) + 'x' + str(ratio[1]) + '-std.npy')
+		sc = 0
+
+		# TODO: Do this in a matrix computation instead?
+		for i in range(ratio[0]):
+			for j in range(ratio[1]):
+				# TODO: to be a probability, this should be multiplied, or add
+				# the logs to prevent numerical issues...
+
+				# avoid n/0 numerical explosion
+				templateStd[i,j] = max(templateStd[i,j],0.001)
+
+				# distance from mean, normalized by standard deviation:
+				d = abs(templateMean[i,j]-shapeDensity[i,j])/templateStd[i,j]
+
+				# Use a sigmoid to kinda approximate the normal CDF
+				sc += 1-1/(1+np.exp(-(6*d-6)))
+
+		# Normalize over different template sizes
+		sc = sc/(ratio[0]*ratio[1])
+		scores.append(sc)
+
+	mxScore = max(scores)
+	if mxScore < 0.4:
+		return 'unclassified'
+	else:
+		return templates[scores.index(mxScore)]
+
+def densityTransform(input,out_size):
+	"""
+	  This maps the input binary image into a smaller grid, and assigns each
+	  block of this smaller grid a value equal to the proportion it is covered
+	  with black squares from the input image.
+	"""
+	# TODO: This should only work when inputsize is greater that outsize in
+	# each dimension.
+	w_in = np.size(input,1)
+	h_in = np.size(input,0)
+	w_out = out_size[1]
+	h_out = out_size[0]
+	output = np.zeros([h_out+1,w_out+1])
+
+	# Get the left/right and top/bottom input square edges into the output
+	# coordinate system.
+	x_edges = np.array(range(w_in+1))*np.double(w_out)/w_in
+	y_edges = np.array(range(h_in+1))*np.double(h_out)/h_in
+
+	w_tr = np.double(w_out)/w_in
+	h_tr = np.double(h_out)/h_in
+
+	dx = np.ceil(x_edges[:-1])-x_edges[:-1]
+	dy = np.ceil(y_edges[:-1])-y_edges[:-1]
+
+	# Add the relevant parts of each square to the final output
+	for i in range(h_in):
+		for j in range(w_in):
+			if input[i,j] == 1:
+				# Add the four divided area elements
+				output[np.floor(y_edges[i]),np.floor(x_edges[j])] += dy[i]*dx[j]
+				output[np.floor(y_edges[i+1]),np.floor(x_edges[j])] += (h_tr-dy[i])*dx[j]
+				output[np.floor(y_edges[i+1]),np.floor(x_edges[j+1])] += (h_tr-dy[i])*(w_tr-dx[j])
+				output[np.floor(y_edges[i]),np.floor(x_edges[j+1])] += dy[i]*(w_tr-dx[j])
+
+	return output[:-1,:-1]
+
+def train(shape,symbol,template_size):
+	"""
+	  This function takes in a shape from the training program, and averages its
+	  density image with the previous stored values for that symbol.
+	"""
+	# Shape is a set of x/y coordinates:
+	shape = np.array(shape)
+	mnpts = np.amin(shape,0)
+	mxpts = np.amax(shape,0)
+	w = mxpts[0] - mnpts[0] + 1
+	h = mxpts[1] - mnpts[1] + 1
+
+	# Transform into a binary image
+	shape_binary = np.zeros([h,w],int)
+	for i in range(np.size(shape,0)):
+		shape_binary[shape[i,1]-mnpts[1],shape[i,0]-mnpts[0]] = 1;
+
+	# Transform the large grid of points into densities the size of the
+	# template
+	new_template = densityTransform(shape_binary,template_size)
+	new_template = np.reshape(new_template,(template_size[0],template_size[1],1))
+
+	name = symbol+'-'+str(template_size[0])+'x'+str(template_size[1])
+
+	try:
+		prev_template = np.load('symbols/' + name + '.npy')
+	except:
+		prev_template = np.zeros([template_size[0],template_size[1],0])
+
+	new_template = np.concatenate((prev_template,new_template),2)
+
+	np.save('symbols/'+name,new_template)
+	np.save('symbols/'+name+'-mean',np.mean(new_template,2))
+	np.save('symbols/'+name+'-std',np.std(new_template,2))
